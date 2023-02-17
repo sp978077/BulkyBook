80524ef60f9792bffd95ed4ea59a2ae1c9e9bcd2
Student <Student@DESKTOP-CUOGL5C>
2023-02-17 13:45:47 +0530

    Add project files.

---
+++ BulkyBook/Controllers/CategoryController.cs
@@ -0,0 +1,28 @@
+﻿using BulkyBook.Data;
+using BulkyBook.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BulkyBook.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+
+        public CategoryController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        public IActionResult Index()
+        {
+            IEnumerable<Category> objCategoryList=_db.Categories.ToList();
+            return View(objCategoryList);
+        }
+
+        //GET
+        public  IActionResult Create()
+        {
+            return View();
+        }
+    }
+}
